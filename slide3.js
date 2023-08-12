9949864cbbb4c06233a85ecbcc2b0a189492a477
ucarcentral001 <142104424+ucarcentral001@users.noreply.github.com>
2023-08-12 22:45:52 +0700

    Create slide3.js

---
+++ slide3.js
@@ -0,0 +1,26 @@
+var index = 0;
+var slides = document.querySelectorAll(".separator");
+var dot = document.querySelectorAll(".dot");
+
+function changeSlide() {
+  if (index < 0) {
+    index = slides.length - 1;
+  }
+
+  if (index > slides.length - 1) {
+    index = 0;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    dot[i].classList.remove("active");
+  }
+
+  dot[index].classList.add("active");
+  
+  index++;
+  
+  setTimeout(changeSlide, 4000);
+}
+
+changeSlide();
